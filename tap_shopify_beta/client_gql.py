bce437b810ee9c43073015af56c736c0e90ef1e5
Key <keynarafaelp@gmail.com>
2024-09-27 15:14:36 -0500

    add sortKey to query request, iterate monthly

--- tap_shopify_beta/client_gql.py
+++ tap_shopify_beta/client_gql.py
@@ -1,7 +1,7 @@
 """GraphQL client handling, including shopifyStream base class."""
 
 from time import sleep
-from typing import Any, Dict, Iterable, List, Optional, Union
+from typing import Any, Dict, Iterable, List, Optional, Union, cast
 
 import requests
 from backports.cached_property import cached_property
@@ -10,7 +10,10 @@ from singer_sdk.streams import GraphQLStream
 import math
 
 from tap_shopify_beta.client import shopifyStream
-
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+import pytz
+import copy
 
 class shopifyGqlStream(shopifyStream):
     """shopify stream class."""
@@ -23,6 +26,8 @@ class shopifyGqlStream(shopifyStream):
     single_object_params = None
     is_list = True
     json_path = None
+    start_date = None
+    end_date = None
 
     @property
     def page_size(self) -> int:
@@ -64,7 +69,7 @@ class shopifyGqlStream(shopifyStream):
         else:
             base_query = """
                 query tapShopify($first: Int, $after: String, $filter: String) {
-                    __query_name__(first: $first, after: $after, query: $filter) {
+                    __query_name__(first: $first, after: $after, query: $filter, sortKey: UPDATED_AT) {
                         edges {
                             cursor
                             node {
@@ -96,21 +101,37 @@ class shopifyGqlStream(shopifyStream):
             cursor_json_path = f"$.data.{self.query_name}.edges[-1].cursor"
             all_matches = extract_jsonpath(cursor_json_path, response_json)
             return next(all_matches, None)
+        elif self.config.get(f"sync_{self.name}_monthly") and not has_next:
+            today = datetime.now().replace(tzinfo=pytz.UTC)
+            if self.end_date < today:
+                self.start_date = self.start_date + relativedelta(months=1)
+                self.logger.info(f"Reached end of data for current month. Moving start date to {self.start_date}")
+                # make self.available_points None so page_size calculation is accurate
+                self.available_points = None
+                return 0
+        self.logger.info(f"Finishing sync for stream {self.name}")
         return None
 
     def get_url_params(
         self, context: Optional[dict], next_page_token: Optional[Any]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
-        params = {}
+        params = dict()
         params["first"] = self.page_size
         if next_page_token:
             params["after"] = next_page_token
         if self.replication_key:
-            start_date = self.get_starting_timestamp(context)
+            start_date = self.start_date or self.get_starting_timestamp(context)
             if start_date:
                 date = start_date.strftime("%Y-%m-%dT%H:%M:%S")
-                params["filter"] = f"updated_at:>{date}"
+                date_filter = f"updated_at:>{date}"
+                # fetch data in monthly chunks
+                if self.config.get(f"sync_{self.name}_monthly"):
+                    self.start_date = start_date
+                    self.end_date = start_date + relativedelta(months=1)
+                    end_date = self.end_date.strftime("%Y-%m-%dT%H:%M:%S")
+                    date_filter = f"{date_filter} AND updated_at:<={end_date}"
+                params["filter"] = date_filter
         if self.single_object_params:
             params = self.single_object_params
         return params
@@ -138,3 +159,54 @@ class shopifyGqlStream(shopifyStream):
 
     def filter_response(self, response_json: dict) -> dict:
         return response_json
+    
+    def prepare_request(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> requests.PreparedRequest:
+        http_method = self.rest_method
+        url: str = self.get_url(context)
+        params: dict = {}
+        request_data = self.prepare_request_payload(context, next_page_token)
+        headers = self.http_headers
+
+        authenticator = self.authenticator
+        if authenticator:
+            headers.update(authenticator.auth_headers or {})
+            params.update(authenticator.auth_params or {})
+
+        request = cast(
+            requests.PreparedRequest,
+            self.requests_session.prepare_request(
+                requests.Request(
+                    method=http_method,
+                    url=url,
+                    params=params,
+                    headers=headers,
+                    json=request_data,
+                ),
+            ),
+        )
+        return request
+
+    def request_records(self, context: Optional[dict]) -> Iterable[dict]:
+        next_page_token: Any = None
+        finished = False
+        decorated_request = self.request_decorator(self._request)
+
+        while not finished:
+            prepared_request = self.prepare_request(
+                context, next_page_token=next_page_token
+            )
+            resp = decorated_request(prepared_request, context)
+            yield from self.parse_response(resp)
+            previous_token = copy.deepcopy(next_page_token)
+            next_page_token = self.get_next_page_token(
+                response=resp, previous_token=previous_token
+            )
+            if next_page_token and next_page_token == previous_token:
+                raise RuntimeError(
+                    f"Loop detected in pagination. "
+                    f"Pagination token {next_page_token} is identical to prior token."
+                )
+            # Cycle until get_next_page_token() no longer returns a value
+            finished = next_page_token is None
