69f55856a1562bcd0c6e7368e0d7b111b11efa2d
Key <keynarafaelp@gmail.com>
2026-06-03 09:22:41 -0400

    use MoneyBagType

--- tap_shopify_beta/types/discount_allocations.py
+++ tap_shopify_beta/types/discount_allocations.py
@@ -1,16 +1,14 @@
 from hotglue_singer_sdk import typing as th
 
 from tap_shopify_beta.types.money_v2 import MoneyV2Type
+from tap_shopify_beta.types.money_bag import MoneyBagType
 from tap_shopify_beta.types.pricing_value import PricingValueType
 
 
 class DiscountAllocationsType(th.ObjectType):
     def __init__(self):
         super().__init__(
-            th.Property("allocatedAmountSet", th.ObjectType(
-                th.Property("presentmentMoney", MoneyV2Type()),
-                th.Property("shopMoney", MoneyV2Type())
-            )),
+            th.Property("allocatedAmountSet", MoneyBagType()),
             th.Property("discountApplication", th.ObjectType(
                 th.Property("allocationMethod", th.StringType),
                 th.Property("targetSelection", th.StringType),
