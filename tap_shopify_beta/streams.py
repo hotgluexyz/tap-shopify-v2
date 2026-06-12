28b3871c9e363474b8c6a0cf82c5cb9d91c9eeed
Otavio Trentin <otavio_trentin@hotmail.com>
2026-06-11 21:10:26 -0400

    remove commented code

--- tap_shopify_beta/streams.py
+++ tap_shopify_beta/streams.py
@@ -971,16 +971,6 @@ class InventoryLevelRestStream(shopifyRestStream):
     primary_keys = ["id"]
     records_jsonpath = "$.inventory_levels.[*]"
 
-    # @property
-    # def add_params(self):
-    #     location_id = self.tap_state.get("bookmarks").get("inventory_level_rest").get("partitions")[-1]["context"]["location_id"]
-    #     params = {"location_ids": location_id}
-    #     if self.config.get("inventory_item_ids"):
-    #         item_ids = self.config.get("inventory_item_ids")
-    #         if isinstance(item_ids,list):
-    #             item_ids = ",".join(item_ids)
-    #         params.update({"inventory_item_ids":item_ids})
-    #     return params
     def get_url_params(self, context, next_page_token):
         params = super().get_url_params(context, next_page_token)
         if next_page_token:
@@ -1022,7 +1012,6 @@ class InventoryLevelGqlStream(shopifyGqlStream):
 
 
     def single_object_params(self, context=None):
-        #inventory_level_id = self.tap_state.get("bookmarks").get("inventory_level_gql").get("partitions")[-1]["context"]["inventory_level_id"]
         return {"id": context["inventory_level_id"]}
 
     schema = th.PropertiesList(
